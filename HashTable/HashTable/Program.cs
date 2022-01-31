8ad3e8fe4501e0d40cef3e17f6e974505accb817
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-31 14:51:47 +0530

    [mahafuz] adding uc3

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -4,27 +4,42 @@ namespace HashTable
 { 
 public class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Welcome to HashTable Program");
-        string paragraph = "To be or not to be";
-        CountWords(paragraph);
-        Console.ReadLine();
-    }
-    /// Counts the words.
-    public static void CountWords(string paragraph)
-    {
-        MyMapNode<string, int> myMapNode = new MyMapNode<string, int>(6);
-        string[] words = paragraph.Split(' ');
-        foreach (string word in words)
+        static readonly MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+        /// Perform Hash operation.
+        public static void HashOperation()
         {
-            if (myMapNode.Exists(word))
-                myMapNode.Add(word.ToLower(), myMapNode.Get(word) + 1);
-            else
-                myMapNode.Add(word.ToLower(), 1);
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "because");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "putting");
+            hash.Add("13", "themselves");
+            hash.Add("14", "deliberately");
+            hash.Add("15", "into ");
+            hash.Add("16", "paranoid");
+            hash.Add("17", "avoidable");
+            hash.Add("18", "situations");
+            string hash0 = hash.Get("0");
+            Console.WriteLine("Oth index value:" + hash0);
+            string hash3 = hash.Get("3");
+            Console.WriteLine("3rd index value:" + hash3);
+            hash.Remove(hash3);
+            string hash10 = hash.Get("10");
+            Console.WriteLine("10th index value:" + hash10);
         }
-        Console.WriteLine("Display after Add operation");
-        myMapNode.Display();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to HashTable Program");
+            HashOperation();
+            Console.ReadLine();
+        }
     }
-}
 }
